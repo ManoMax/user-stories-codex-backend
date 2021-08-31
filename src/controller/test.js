b927268eed6bad3cd0229dd771eda1ee261ca05e
ManoMax <gabrielmaxcontato@gmail.com>
2021-08-31 19:03:01 -0300

    feat: creating get and post users, and routes

--- src/controller/test.js
+++ src/controller/test.js
@@ -1,7 +1,31 @@
+const User = require('../model/User');
+
 const TestController = {
   helloWorld(req, res) {
     res.send('Hello World')
   },
+
+  async index(request, response) {
+    const users = await User.find();
+
+    return response.json(users);
+  },
+
+  async store(request, response) {
+    const { name, email, password } = request.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        name,
+        email,
+        password
+      });
+    }
+
+    return response.json(user);
+  }
 };
 
 module.exports = TestController;
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,9 @@ const router = require('express').Router();
 const TestController = require('@controller/test');
 
 router.get('/', TestController.helloWorld);
+
+router.post('/users', TestController.store);
+
+router.get('/users', TestController.index);
+
 module.exports = router;
